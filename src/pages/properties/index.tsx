8477cb35ca9e20a1f3cf00e3ed9ba4826bbd85fa
Vijay Maurya <126171677+vjzest@users.noreply.github.com>
2025-05-12 12:11:53 +0530

    Update index.tsx

--- src/pages/properties/index.tsx
+++ src/pages/properties/index.tsx
@@ -7,7 +7,7 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { Search } from 'lucide-react';
 import { Input } from '@/components/ui/input';
-const BASE_URL = "https://propb1.onrender.com";
+const BASE_URL = "https://propb1.onrender.co";
 
 const PropertyCard = ({ property }: { property: any }) => {
   const [showDialog, setShowDialog] = useState(false);
